6b5a9501b300a199c1558b45052e4ddbff5d00a3
joel langat <langatjoel42@gmail.com>
2024-03-21 12:47:13 +0300

    made some functions in index.js

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"] ;
+
+function destructivelyAppendCat(name){
+    cats.push("Ralph")
+}
+
+function destructivelyPrependCat(name){
+    cats.unshift("Bob")
+}
+
+function destructivelyRemoveLastCat(){
+     cats.pop();
+}
+
+function destructivelyRemoveFirstCat(){
+    cats.shift();
+}
+
+function appendCat(name){
+    const allCats = [...cats, "Broom"];
+    return [...cats,"Broom"]
+}
+
+function prependCat(name){
+    const allCats = ["Arnold", ...cats];
+    return ["Arnold",...cats];
+
+}
+
+function removeLastCat(){
+   return cats.slice(0, -1);
+}
+
+function removeFirstCat(){
+    return cats.slice(1)
+
+}
